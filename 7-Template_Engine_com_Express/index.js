3509d24ee8232d72f72a3ffcaa1e025d3e37daee
Talison-Miguel <talisonmiguel84@gmail.com>
2025-01-02 23:04:21 -0300

    Finalizando seção do handlebars

--- 7-Template_Engine_com_Express/index.js
+++ 7-Template_Engine_com_Express/index.js
@@ -3,8 +3,14 @@ const exphbs = require('express-handlebars')
 
 const app = express()
 
-app.engine('handlebars', exphbs.engine())
+const hbs = exphbs.create({
+    partialsDir: ['view/partials']
+})
+
+app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
 
 app.get('/dashboard', (req, res) => {
 
@@ -21,6 +27,31 @@ app.get('/post', (req, res) => {
     }
 
     res.render('blogpost', { post })
+})
+
+app.get("/blog", (req, res) => {
+    const posts = [
+        {
+            title: 'Aprender Node.JS',
+            category: 'JS',
+            body: 'corpo do texto',
+            comments: 4,
+        },
+        {
+            title: 'Aprender PHP',
+            category: 'JS',
+            body: 'corpo do texto',
+            comments: 8,
+        },
+        {
+            title: 'Aprender PY',
+            category: 'JS',
+            body: 'corpo do texto',
+            comments: 2,
+        },
+    ]
+
+    res.render("blog", {posts})
 })
 
 app.get('/', (req, res) => {
