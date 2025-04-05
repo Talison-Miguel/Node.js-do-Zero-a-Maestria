e6f1706b5f625cb9e7762b039d55297191e0b275
Talison-Miguel <talisonmiguel84@gmail.com>
2025-04-05 17:34:46 -0300

    feat: Iniciando projeto

---
+++ 11-projeto-thoughts/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require ('sequelize')
+
+const sequelize = new Sequelize('projetoCursoNode', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try {
+    sequelize.authenticate()
+    console.log('Conectado em: localhost://3000')
+} catch(err) {
+    console.log(err)
+}
+
+module.exports = sequelize
